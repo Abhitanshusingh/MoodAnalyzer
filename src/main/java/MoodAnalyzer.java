8ac68dfd9db74c058e0c30ccf3086c2e8a3bdfb1
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-06 17:24:27 +0530

    Added:checking happy or sad mood

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class MoodAnalyzer {
     public static void main(String[] args) {
         System.out.println("Welcome to mood analyzer");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your mood");
+        String mood=sc.nextLine();
+        sc.close();
+        System.out.println(moodAnalyse(mood));
+    }
+    public static String moodAnalyse(String message) {
+        if (message.length() == 0) {
+            System.out.println("Enter mood");
+        }
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
