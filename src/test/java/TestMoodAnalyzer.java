3ef74cd191a7a445d68cc064333f97a4d79e2637
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-06 18:52:36 +0530

    REFACTOR: Added two constructor

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -2,17 +2,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMoodAnalyzer {
-    MoodAnalyzer analyzer = new MoodAnalyzer();
+    MoodAnalyzer analyzer;
+
     //Analyse Mood IS SAD
     @Test
     public void moodAnalyzerclass_WhenMessageIncludeSad_ShouldReturnSad() {
-        String result = analyzer.moodAnalyse("I am in sad mood");
-        Assert.assertEquals("SAD", result);
+        analyzer = new MoodAnalyzer("I am in sad mood");
+        Assert.assertEquals("SAD", analyzer.analyseMood());
     }
+
     //Analyse Mood IS HAPPY
     @Test
-    public void moodAnalyzerclass_WhenMessageIncludeHappy_ShouldReturnHappy(){
-        String result = analyzer.moodAnalyse("I am in happy mood");
-        Assert.assertEquals("HAPPY", result);
+    public void moodAnalyzerclass_WhenMessageIncludeHappy_ShouldReturnHappy() {
+        analyzer = new MoodAnalyzer("I am in happy mood");
+        Assert.assertEquals("HAPPY", analyzer.analyseMood());
     }
 }
