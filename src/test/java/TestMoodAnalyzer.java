c6dd41672f254b96331c0f9796810d571884d886
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-06 17:26:52 +0530

    Added: Analyse mood method test case passed

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestMoodAnalyzer {
+    MoodAnalyzer analyzer = new MoodAnalyzer();
+    //Analyse Mood
+    @Test
+    public void givenMoodAnalyzerClass_whenProper_ShouldRrturnObject() {
+        String result = analyzer.moodAnalyse("I am in sad mood");
+        Assert.assertEquals("SAD", result);
+    }
 }
