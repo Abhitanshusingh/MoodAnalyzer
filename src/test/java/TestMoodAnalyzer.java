6ec04f4c979571ae3438ece2de7c3d012164bb24
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-07 15:11:30 +0530

    ADDED: Passed test case happy and sad mood

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -4,14 +4,12 @@ import org.junit.Test;
 public class TestMoodAnalyzer {
     MoodAnalyzer analyzer;
 
-    //Analyse Mood IS SAD
     @Test
     public void moodAnalyzerclass_WhenMessageIncludeSad_ShouldReturnSad() {
         analyzer = new MoodAnalyzer("I am in sad mood");
         Assert.assertEquals("SAD", analyzer.analyseMood());
     }
 
-    //Analyse Mood IS HAPPY
     @Test
     public void moodAnalyzerclass_WhenMessageIncludeHappy_ShouldReturnHappy() {
         analyzer = new MoodAnalyzer("I am in happy mood");
