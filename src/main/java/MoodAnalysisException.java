f34067c29e3ff8f614bd3d3f076565ebc1f6258c
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-09 14:59:49 +0530

    ADDED: Custom Exception

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,2 +1,13 @@
 public class MoodAnalysisException extends Exception {
+    enum ExceptionType {
+        ENTERED_NULL, ENTERED_EMPTY;
+    }
+
+    ExceptionType type;
+
+    public MoodAnalysisException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
 }
