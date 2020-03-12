75c0f83e2c0014a7f5187d9d666a2508494424b0
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-12 13:16:31 +0530

    MODIFIED: Handled exception when user entered improper class name

--- src/main/java/MoodAnalyzerFactory.java
+++ src/main/java/MoodAnalyzerFactory.java
@@ -1,12 +1,24 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-public class MoodAnalyzerFactory
-{
-    public static MoodAnalyzer createMoodAnalyserObject() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Class <?> moodAnalyze=Class.forName("MoodAnalyzer");
-        Constructor<?> moodAnalysisConstructor = moodAnalyze.getConstructor();
-        Object moodObject = moodAnalysisConstructor.newInstance();
-         return  (MoodAnalyzer) moodObject;
+public class MoodAnalyzerFactory {
+    public static MoodAnalyzer createMoodAnalyserObject() throws MoodAnalysisException {
+        try {
+            Class<?> moodAnalyze = Class.forName("MoodyAnalyzer");
+            Constructor<?> moodAnalysisConstructor = moodAnalyze.getConstructor();
+            Object moodObject = moodAnalysisConstructor.newInstance();
+            return (MoodAnalyzer) moodObject;
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "No class");
+        } catch (NoSuchMethodException e) {
+           e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
