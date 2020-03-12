8459f5bd1cb12e83e93171d45dfe9ee7da7a9aa3
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-12 12:26:38 +0530

    ADDED: Comparing two object and test case passed

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -1,40 +1,48 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class TestMoodAnalyzer {
     MoodAnalyzer analyzer;
 
     @Test
-    public void moodAnalyzerclass_WhenMessageIncludeSad_ShouldReturnSad() throws MoodAnalysisException {
+    public void moodAnalyzer_WhenMessageIncludeSad_ShouldReturnSad() throws MoodAnalysisException {
         analyzer = new MoodAnalyzer("I am in sad mood");
         Assert.assertEquals("SAD", analyzer.analyseMood());
     }
 
     @Test
-    public void moodAnalyzerclass_WhenMessageIncludeHappy_ShouldReturnHappy() throws MoodAnalysisException {
+    public void moodAnalyzer_WhenMessageIncludeHappy_ShouldReturnHappy() throws MoodAnalysisException {
         analyzer = new MoodAnalyzer("I am in happy mood");
         Assert.assertEquals("HAPPY", analyzer.analyseMood());
     }
 
     @Test
-    public void moodAnalyzerclass_WhenMessageIncludeNull_ShouldThrowException(){
+    public void moodAnalyzer_WhenMessageIncludeNull_ShouldThrowException() {
         analyzer = new MoodAnalyzer(null);
         try {
             analyzer.analyseMood();
-        }
-        catch (MoodAnalysisException e){
-            Assert.assertEquals("Please enter proper mood",e.getMessage());
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals("Please enter proper mood", e.getMessage());
         }
     }
 
     @Test
-    public void moodAnalyzerclass_WhenMessageIncludeEmpty_ShouldThrowException(){
+    public void moodAnalyzer_WhenMessageIncludeEmpty_ShouldThrowException() {
         analyzer = new MoodAnalyzer("");
         try {
             analyzer.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals("Please enter proper mood", e.getMessage());
         }
-        catch (MoodAnalysisException e){
-            Assert.assertEquals("Please enter proper mood",e.getMessage());
-        }
+    }
+
+    @Test
+    public void moodAnalyzer_WhenProper_ShouldReturnObject() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        analyzer=new MoodAnalyzer();
+        MoodAnalyzer moodAnalyzerObject=MoodAnalyzerFactory.createMoodAnalyserObject();
+        boolean result =analyzer.equals(moodAnalyzerObject);
+        Assert.assertEquals(true,result);
     }
 }
