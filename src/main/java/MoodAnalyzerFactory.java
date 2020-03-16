5aa4012dc80a2debc1dee1272bd51cba4c81cd10
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-16 11:05:53 +0530

    ADDED: setFieldValue method to assert value

--- src/main/java/MoodAnalyzerFactory.java
+++ src/main/java/MoodAnalyzerFactory.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -41,4 +42,17 @@ public class MoodAnalyzerFactory {
         }
         return null;
     }
+
+    public static void SetFieldValue(Object moodObject, String fieldName, String fieldValue) {
+        Class<?> className = moodObject.getClass();
+        try {
+            Field field = className.getDeclaredField(fieldName);
+            field.set(moodObject, fieldValue);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
